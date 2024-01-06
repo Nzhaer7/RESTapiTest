44589c0b974fc41201372314572190ce535946b6
Nzhaer7 <suleymanogrk24@gmail.com>
2024-01-06 18:35:52 +0300

    initial commit

---
+++ api/routes/orders.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "order is fetched",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const order = {
+    productID: req.body.productID,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({
+    message: "order is created",
+    order: order,
+  });
+});
+
+router.get("/:orderID", (req, res, next) => {
+  res.status(200).json({
+    message: "order details",
+    orderID: req.params.orderID,
+  });
+});
+
+router.delete("/:orderID", (req, res, next) => {
+  res.status(200).json({
+    message: "order deleted",
+    orderID: req.params.orderID,
+  });
+});
+
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
+
+router.get("/", (req, res, next) => {
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json({ docs });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "product post",
+        createdProduct: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/:productID", (req, res, next) => {
+  const id = req.params.productID;
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log("from db", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: "no valid entry for porvided ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.patch("/:productID", (req, res, next) => {
+  const id = req.params.productID;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.findOneAndUpdate({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: err });
+    });
+});
+
+router.delete("/:productID", (req, res, next) => {
+  const id = req.params.productID;
+  Product.findByIdAndDelete({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
+});
+
+module.exports = router;
